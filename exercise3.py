3a43f01a87578771add4d2e5805790b375537138
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 12:23:54 -0400

    Shaughn

--- exercise3.py
+++ exercise3.py
@@ -1,36 +1,37 @@
-# Jordan Griffin (jordangriffin800)
-# COP2002-0T1
-# September 23, 2024
-# Exercise 3: If Statements
-# Create a program that asks students to enter a hex digit code from a NIC card to see which manufacturer it belongs to.
-
-print("MAC Manufacturer Program")
-
-print("------------------------")
-
-print()
-
-# get OUI from user
-
-userHexDigits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-
-if("00:00:17" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Oracle.")
-
-elif("00:07:E9" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Intel Corporation.")
-
-elif("04:27:28" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Microsoft Corporation.")
-
-elif("04:26:65" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Apple, Inc.")
-
-elif("04:33:89" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Huawei Technologies Co.,Ltd.")
-
-elif("00:00:0C" == userHexDigits):
-    print(f"For {userHexDigits} the MAC manufacturer is Cisco Systems, Inc.")
-
-else:
-    print("Unknown")
+#Linuxhacker07
+#COP2002.0T1 Exercise 2: Variables
+#September 25, 2024
+#Mac Manufacture Program
+#program tells user what mac manufacturer is.
+print("Mac Manufacture Program")
+print("-----------------------")
+print("Type exit to leave")
+print("")
+print("")
+def main():
+    MacNum = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    if MacNum == "00:00:17":
+        print(f"For {MacNum} the MAC manufacturer is Oracle.")
+        main()
+    elif MacNum == "00:07:E9":
+        print(f"For {MacNum} the MAC manufacturer is Intel Corporation.")
+        main()
+    elif MacNum == "04:27:28":
+        print(f"For {MacNum} the MAC manufacturer is Microsoft Corporation.")
+        main()
+    elif MacNum == "04:26:65":
+        print(f"For {MacNum} the MAC manufacturer is Apple, Inc.")
+        main()
+    elif MacNum == "04:33:89":
+        print(f"For {MacNum} the MAC manufacturer is Huawei Technologies Co.,Ltd.")
+        main()
+    elif MacNum == "00:00:0C":
+        print(f"For {MacNum} the MAC manufacturer is Cisco Systems, Inc.")
+        main()
+    elif MacNum == "exit":
+        print("Goodbye!")
+    else:
+        print("Unknown")
+        main()
+if(__name__=="__main__"):
+    main()
