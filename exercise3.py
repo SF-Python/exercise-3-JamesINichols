c4f1581646b476931ce35a4ca7d468f7ddb5a87c
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 10:25:42 -0400

    Jordan G

--- exercise3.py
+++ exercise3.py
@@ -1,37 +1,36 @@
-# Jaden Donda
-# F24 COP2002.0T1: PROGRAM LOGIC
-# 9/21/24
-# MAC Manufacturer Program - A program that asks the user for the first 6 hex digits formatted as XX:XX:XX and provides the manufacturer
+# Jordan Griffin (jordangriffin800)
+# COP2002-0T1
+# September 23, 2024
+# Exercise 3: If Statements
+# Create a program that asks students to enter a hex digit code from a NIC card to see which manufacturer it belongs to.
 
-def main():
-   # creating chart to connect hex digits to manufacturers
-    mac_manufacturers = {
-        "00:00:17": "Oracle",
-        "00:07:E9": "Intel Corporation",
-        "04:27:28": "Microsoft Corporation",
-        "04:26:65": "Apple, Inc.",
-        "04:33:89": "Huawei Technologies Co.,Ltd",
-        "00:00:0C": "Cisco Systems, Inc"
-    }
+print("MAC Manufacturer Program")
 
-       # program title
-    print("MAC Manufacturer Program")  # MAC output statement
-    print("-------------------------")  # dashes output statement
-    print()  # (blank line)
-    
-       # asking for MAC address using the first 6 hex digits
-    mac_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ").upper()
+print("------------------------")
 
-       # find manufacturer based on the provided input
-    manufacturer = mac_manufacturers.get(mac_input, "< Invalid value or not found>")
-    
-       # display result
-    if manufacturer != "<Invalid value or not found>":
-        print(f"\nFor {mac_input}, the MAC manufacturer is {manufacturer}.")
-    else:
-        print(f"\n{mac_input} is invalid or not found.")
+print()
 
-    # calling main function
-if __name__ == "__main__":
-    main()
+# get OUI from user
 
+userHexDigits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+
+if("00:00:17" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Oracle.")
+
+elif("00:07:E9" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Intel Corporation.")
+
+elif("04:27:28" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Microsoft Corporation.")
+
+elif("04:26:65" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Apple, Inc.")
+
+elif("04:33:89" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Huawei Technologies Co.,Ltd.")
+
+elif("00:00:0C" == userHexDigits):
+    print(f"For {userHexDigits} the MAC manufacturer is Cisco Systems, Inc.")
+
+else:
+    print("Unknown")
